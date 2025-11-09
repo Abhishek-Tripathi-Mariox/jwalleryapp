f39a2a62b8552a54dfa23f89a8ee656417961a85
Jyoti <jyotipatel2138@gmail.com>
2025-11-09 23:16:26 +0530

    jyoti

--- src/screens/SplashScreen/controller.js
+++ src/screens/SplashScreen/controller.js
@@ -1,7 +1,14 @@
+import { getTokenStorage } from '../../utils/tokenStorage';
+
 const TimeOut = (props) => {
     const { navigation } = props;
-    const timer = setTimeout(() => {
-        navigation.navigate('Landing');
+    const timer = setTimeout(async () => {
+        const token = await getTokenStorage();
+        if (token) {
+            navigation.navigate('Home');
+        } else {
+            navigation.navigate('Landing');
+        }
     }, 3000);
 
     return () => clearTimeout(timer);
